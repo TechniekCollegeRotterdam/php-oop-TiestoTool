542b219c82620f48f369ca09f4f34f4d9f9d799d
TiestoTool <9019545@student.zadkine.nl>
2023-11-22 14:55:58 +0100

    opdracht 4
    ikke heb opdracht 4 af hoor jason

--- Person.php
+++ Person.php
@@ -5,16 +5,21 @@ class Person
     public $name;
     public $age;
     public $money = 4000;
-
+    public  function setName($name){
+        $this->name = ucfirst($name);
+    }
     public function formatMoney(){
         return number_format($this->money, 2);
     }
 }
 
 $person1 = new Person();
-echo $person1-> name ="Rouhi". "<br>";
+$person1->setName("rouhi");
+echo $person1->name . "<br>";
 echo $person1->formatMoney(). "<br>";
+
 $person2 = new Person();
-echo $person2-> name = "Karim". "<br>";
-$person2-> money = 5000 . "<br>";
+echo $person2-> name = "karim". "<br>";
+$person2-> money = 5000 ;
+
 echo $person2->formatMoney();
