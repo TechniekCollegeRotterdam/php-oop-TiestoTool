1e1ed2a05192caf570ffb97dead8d2aec5d7c540
TiestoTool <9019545@student.zadkine.nl>
2023-12-01 10:28:49 +0100

    Update Product.php

--- Product.php
+++ Product.php
@@ -23,14 +23,19 @@
          return number_format($this->price, 2);
      }
 
+     public function getProduct()
+     {
+        return"Het product is ".$this->name. "kost ". $this->currency. " ". $this->price;
+     }
  }
 
  $drank1 = new Product(30);
- echo $drank1->category . " " . $drank1 ->rating;
-$drank2 = new Product(30);
-echo $drank2->category . " " . $drank2 ->rating;
-$drank3 = new Product(30);
-echo $drank3->category . " " . $drank3 ->rating;
+ echo $drank1->getProduct();
+// echo $drank1->category . " " . $drank1 ->rating;
+//$drank2 = new Product(30);
+//echo $drank2->category . " " . $drank2 ->rating;
+//$drank3 = new Product(30);
+//echo $drank3->category . " " . $drank3 ->rating;
 
 
 
